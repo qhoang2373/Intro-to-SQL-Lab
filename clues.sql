444fcb2e747350c6685c242c6e9dec6448a6d195
Quan <qhoang2373@gmail.com>
2025-02-17 21:27:41 -0800

    exercise 6 complete

--- clues.sql
+++ clues.sql
@@ -53,7 +53,9 @@ WHERE region = 'South America' and cities.name LIKE '%San Mar%';
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
-
+SELECT district -- retrieve the district column from the cities table
+FROM cities -- look in the cities table that holds information about the different cities, 
+WHERE name LIKE 'General San Mart%'; -- filter condition applied to the query
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
